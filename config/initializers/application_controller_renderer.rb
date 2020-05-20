367a9145bafdcf212decef26aa420c8ea10fc66d
mst827 <k.s.m501196gmail.com>
2020-05-21 08:52:46 +0900

    done

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '147b8d89c7ac32fba0ec82e5125e7d9e3c951154d22ed4f129285bbf2ab59987ad1243003f6497400a7faf8c020341d2cd198e169e1dff8add3bd41e78b5a0ce'
